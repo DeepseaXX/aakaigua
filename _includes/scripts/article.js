7cfb59c8570ece1c8b459dd8af3c8cb7d8600d5b
DeepseaXX <33820091+DeepseaXX@users.noreply.github.com>
2026-05-10 14:22:22 +0900

    Try copy button

--- _includes/scripts/article.js
+++ _includes/scripts/article.js
@@ -11,6 +11,30 @@
       $articleContent.find('.highlight').each(function() {
         $this = $(this);
         $this.attr('data-lang', $this.find('code').attr('data-lang'));
+        var $copyBtn = $('<button class="copy-button d-print-none" title="复制代码"><i class="far fa-copy"></i></button>');
+        $this.append($copyBtn);
+      });
+
+      $articleContent.on('click', '.copy-button', function() {
+        var $btn = $(this);
+        var $highlight = $btn.closest('.highlight');
+        var code = '';
+        var $table = $highlight.find('table.rouge-table');
+        
+        if ($table.length > 0) {
+          code = $table.find('.code').text();
+        } else {
+          code = $highlight.find('code').text();
+        }
+
+        navigator.clipboard.writeText(code).then(function() {
+          $btn.html('<i class="fas fa-check"></i>');
+          setTimeout(function() {
+            $btn.html('<i class="far fa-copy"></i>');
+          }, 2000);
+        }).catch(function(err) {
+          console.error('Failed to copy: ', err);
+        });
       });
       $articleContent.find('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').each(function() {
         $this = $(this);
